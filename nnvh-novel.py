be224ca928ff26b3c118b29b2c75b716c6101e16
Khoi Nguyen Tuan <khoikute78456@gmail.com>
2021-07-22 16:19:18 +0700

    finish the scraper but without writing to PDF file

--- nnvh-novel.py
+++ nnvh-novel.py
@@ -1,14 +1,32 @@
 import requests
 from bs4 import BeautifulSoup
+base_url = "https://bachngocsach.com"
+url = "https://bachngocsach.com/reader/nhat-niem-vinh-hang/yfrd"
+html = requests.get(url)
+soup = BeautifulSoup(html.content, "html.parser")
 
-# input_url = input("Paste the your chapter URL: ")
-# input_character = int(input("Enter the chapter number: "))
-url = requests.get(
-    "https://truyenyy.vip/truyen/nhat-niem-vinh-hang-dich/chuong-1350.html")
-soup = BeautifulSoup(url.content, "html.parser")
+CHAPTERS = 500
+with open("Nhat-niem-vinh-hang.txt", "w", encoding="utf-8") as output:
+    for i in range(CHAPTERS):
+        title = soup.find("h1", id="chuong-title")
+        content = soup.find("div", id="noi-dung")
+        output.write(title.get_text() + "\n\n\n")
+        for para in content.find_all("p"):
+            output.write(para.get_text() + "\n\n")
+            pass
 
+        output.write("\n\n\n\n")
+        try:
+            next_chapter_button = soup.find(
+                "a", class_="page-next captainicon chuong-button")
+            url = base_url + next_chapter_button['href']
+            html = requests.get(url)
+            soup = BeautifulSoup(html.content, "html.parser")
 
-CHAPTERS = 100
+        except TypeError:
+            print("The END!")
+            break
+    pass
 # # for i in range(CHAPTERS):
 # parent_content = soup.find_all("div", class_="inner")
 # title = soup.find_all("h1", class_="chapter-title")
